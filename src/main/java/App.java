eff88e2477191d35e5ed5762cece9ef25189564e
Pavel Kozhaev <zxc321z9@gmail.com>
2021-10-20 21:47:40 +0300

    Выполнены условия задания 212
    - добавлен класс Cat
    - добавлен бин для Cat в AppConfig с указанием области видимости бина
    - пройден успешно тест
    
    https://platform.kata.academy/user/courses/3/2/1/2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,17 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld helloWorldBean = (HelloWorld) applicationContext.getBean("helloWorld");
+        HelloWorld twoHelloWorldBean = (HelloWorld) applicationContext.getBean("helloWorld");
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat twoCatBean = (Cat) applicationContext.getBean("cat");
+
+        boolean helloWorldBeanCompare = helloWorldBean == twoHelloWorldBean;
+        boolean catBeanCompare = catBean == twoCatBean;
+
+        System.out.println(helloWorldBeanCompare);
+        System.out.println(catBeanCompare);
     }
 }
